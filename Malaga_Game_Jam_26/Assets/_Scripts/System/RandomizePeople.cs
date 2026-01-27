a306410fd4e66d12de7114efcd5b7ad5d327ad78
knightflo <f.van.den.berg@sintlucasedu.nl>
2026-01-27 10:11:47 +0100

    Added people randomized

--- Malaga_Game_Jam_26/Assets/_Scripts/System/RandomizePeople.cs
+++ Malaga_Game_Jam_26/Assets/_Scripts/System/RandomizePeople.cs
@@ -7,12 +7,29 @@ public class RandomizePeople : MonoBehaviour
     [SerializeField] private List<string> lastNames = new List<string>();
     [SerializeField] private List<string> personalities = new List<string>();
 
+    private List<People.Imigrant> imigrants = new List<People.Imigrant>();
 
-    public void RandomizeImmigrant()
+    [SerializeField] private float immigrantAmount;
+
+    private void Start()
+    {
+        addImmigrants();
+    }
+    private void addImmigrants()
+    {
+        for (int i = 0; i < immigrantAmount; i++)
+        {
+            RandomizeImmigrant();
+        }
+    }
+    private void RandomizeImmigrant()
     {
         string name = names[Random.Range(0, names.Count)];
         string lastName = lastNames[Random.Range(0, lastNames.Count)];
         string personality = personalities[Random.Range(0, personalities.Count)];
         bool isZombie = Random.Range(0, 2) == 1;
+
+        People.Imigrant person = new People.Imigrant(name, lastName, personality, isZombie);
+        imigrants.Add(person);
     }
 }

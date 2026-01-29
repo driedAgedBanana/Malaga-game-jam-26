c73283790fd13abfe3a3a832186af168ecad4581
Bas Pennings <p.freekje@gmail.com>
2026-01-29 12:57:34 +0100

    Updated ImmigrantVisualizer based on provided assets

--- Malaga_Game_Jam_26/Assets/_Scripts/People/ImmigrantVisualizer.cs
+++ Malaga_Game_Jam_26/Assets/_Scripts/People/ImmigrantVisualizer.cs
@@ -5,12 +5,16 @@ using static People;
 public class ImmigrantVisualizer : MonoBehaviour
 {
     [Header("References to object images")]
+    [SerializeField] private Image _baseImg;
     [SerializeField] private Image _eyesImg;
-    [SerializeField] private Image _jacketImg;
+    [SerializeField] private Image _maskImg;
+    [SerializeField] private Image _clothingImg;
 
     public void Visualize(Imigrant imigrant)
     {
+        _baseImg.image = imigrant.BaseImg;
         _eyesImg.image = imigrant.Eyes;
-        _jacketImg.image = imigrant.Jacket;
+        _maskImg.image = imigrant.BaseImg;
+        _clothingImg.image = imigrant.Jacket;
     }
 }
--- Malaga_Game_Jam_26/Assets/_Scripts/People/People.cs
+++ Malaga_Game_Jam_26/Assets/_Scripts/People/People.cs
@@ -8,20 +8,21 @@ public class People : MonoBehaviour
         public string LastName;
         public string Personality;
         public bool IsZombie;
-        public Texture2D Image;
+        public Texture2D BaseImg;
         public Texture2D Eyes;
         public Texture2D Jacket;
 
         // Constructor
-        public Imigrant(string firstName, string lastName, string personality, bool isZombie, Texture2D image, Texture2D eyes, Texture2D jacket)
+        public Imigrant(string firstName, string lastName, string personality, bool isZombie, Texture2D image, Texture2D eyes, Texture2D jacket, Texture2D baseImg)
         {
             FirstName = firstName;
             LastName = lastName;
             Personality = personality;
             IsZombie = isZombie;
-            Image = image;
+            BaseImg = image;
             Eyes = eyes;
             Jacket = jacket;
+            BaseImg = baseImg;
         }
     }
 
